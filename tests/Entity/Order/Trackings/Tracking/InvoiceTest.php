26ecee09761a6a26725754498e0b21ec80c46cd3
Gilmar Pupo <g@g1mr.com>
2015-05-06 16:45:35 -0300

    Testes+

--- tests/Entity/Order/Trackings/Tracking/InvoiceTest.php
+++ tests/Entity/Order/Trackings/Tracking/InvoiceTest.php
@@ -23,5 +23,32 @@ class InvoiceTest extends OrderTestCaseAbstract
         self::displayClassDocumentation(new Invoice());
     }
 
-    
+    protected function factoryInvoice()
+    {
+        return new Invoice([
+            'cnpj'      => '111111111111',
+            'number'    => '2456',
+            'serie'     => '1',
+            'issuedAt'  => '2015-05-04T14:54:29.000-03:00',
+            'accessKey' => 'fooBarZetaJones',
+            'linkXML'   => 'http://foo/bar',
+            'linkDanfe' => 'http://bar/foo',
+        ]);
+    }
+
+    public function testPossuiCnpj()
+    {
+        $invoice = $this->factoryInvoice();
+        $this->assertEquals('111111111111', $invoice->getCnpj());
+
+        return $invoice;
+    }
+
+    /**
+     * @depends testPossuiCnpj
+     */
+    public function testPossuiChaveDeAcesso(Invoice $invoice)
+    {
+        $this->assertEquals('fooBarZetaJones', $invoice->getAccessKey());
+    }
 }
